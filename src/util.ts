0b2736560dc60643ba367faf4ea85dcf6af744f6
Kaden Scott <69081152+kadenscott@users.noreply.github.com>
2020-12-23 03:07:56 -0500

    fix development paths

--- src/util.ts
+++ src/util.ts
@@ -2,5 +2,5 @@ export const isProduction = () => {
     return process.env.PRODUCTION !== undefined && process.env.PRODUCTION === "true"
 }
 
-export const DATA_DIR = isProduction() ? "/app/data" : "/home/kscott/projects/bluebox/app/data"
-export const PUBLIC_DIR = isProduction() ? "/app/public" : "/home/kscott/projects/bluebox/app/public"
+export const DATA_DIR = isProduction() ? "/app/data" : "/home/kscott/projects/bluebox/data"
+export const PUBLIC_DIR = isProduction() ? "/app/public" : "/home/kscott/projects/bluebox/public"
